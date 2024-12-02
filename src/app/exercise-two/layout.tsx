c063a3b31b60682b6f24f5a33c41a8d347a2cd64
Juan Manuel <juanmanuel@MacBook-Pro-de-Juan.local>
2024-12-02 17:47:14 +0100

    fix imports

--- src/app/exercise-two/layout.tsx
+++ src/app/exercise-two/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // Providers
 import Provider from "../../lib/Provider";
 // Styles
-import "../assets/globals.css";
+import "../../assets/globals.css";
 // Components
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
